a3a679175842c76b2f226ca2e10773b69c0679e4
lhppeng <62881286+lhppeng@users.noreply.github.com>
2025-02-27 17:11:24 +0800

    Add files via upload

--- plugins/gotifymsglhp/__init__.py
+++ plugins/gotifymsglhp/__init__.py
@@ -22,7 +22,7 @@ class GotifyMsgLhp(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/gotify/logo/refs/heads/master/gotify-logo.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "lhp"
     # 作者主页
